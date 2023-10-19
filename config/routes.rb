43b2056dabe73411a45d68c7d1c451a6aac53c92
karu1325 <143956677+karu1325@users.noreply.github.com>
2023-10-19 00:55:32 +0000

    [Add]admin_login

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
+  root to: 'public/homes#top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
