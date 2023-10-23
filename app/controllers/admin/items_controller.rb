f467cf97bc13464856fcbd797b0ae7264280b821
karu1325 <143956677+karu1325@users.noreply.github.com>
2023-10-23 15:17:05 +0000

    [Add]admin-item-new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,32 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def new
+    @item = Item.new
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @item = Item.new
+    @item.save
+    redirect_to admin_item_path(@item.id)
+  end
+
+  def update
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_sale, :image)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,9 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
   belongs_to :genre
+  has_one_attached :image
+
+  def get_image(width,height)
+    image.variant(resize_to_limit:[width,height]).processed
+  end
 end
