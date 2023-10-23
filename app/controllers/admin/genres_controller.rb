3947c3dfedf884d89632689568cc1a6752406993
karu1325 <143956677+karu1325@users.noreply.github.com>
2023-10-23 04:27:14 +0000

    [Add]genre-index.new,admingenre/before_action

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,15 +1,27 @@
 class Admin::GenresController < ApplicationController
-
+before_action :authenticate_admin!
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
   end
 
   def update
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 
 
