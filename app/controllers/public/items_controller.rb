f9ed677a76feef84f5fa295dfb25e7d5cac2f5dd
karu1325 <143956677+karu1325@users.noreply.github.com>
2023-10-25 16:56:22 +0000

    [Add]public-item-index

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,17 @@
 class Public::ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+    @items = Item.page(params[:page]).per(8)
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :genre_id)
+  end
 end
