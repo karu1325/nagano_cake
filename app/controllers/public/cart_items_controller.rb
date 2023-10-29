86b25dfdaa9df5499b08622eac2e82947701f68b
karu1325 <143956677+karu1325@users.noreply.github.com>
2023-10-29 16:21:44 +0000

    [Add]cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,13 +2,22 @@ class Public::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items
-    @total = @cart_items.inject(0) { |sum, item| sum + item.with_tax_price }
+    @total = 0
   end
 
   def create
-    cart_item = current_customer.cart_items.new(cart_item_params)
-    cart_item.save
-    redirect_to cart_items_path
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    elsif @cart_item.save
+      redirect_to cart_items_path
+    else
+      @cart_items = current_customer.cart_items
+      render 'index'
+    end
   end
 
   def update
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :order
   has_many :order_details, dependent: :destroy
+  
 
   enum status: {
     waiting: 0,
