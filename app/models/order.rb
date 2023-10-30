5206c9848ba5341295af12fb8589c392f4f23fd2
karu1325 <143956677+karu1325@users.noreply.github.com>
2023-10-30 03:29:20 +0000

    [Add]enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :order
   has_many :order_details, dependent: :destroy
-  
+
 
   enum status: {
     waiting: 0,
@@ -13,7 +13,7 @@ class Order < ApplicationRecord
 
   enum payment_method: {
     credit: 0,
-    bank: 1
+    transfer: 1
   }
 
 end
