624465bf33e41717c9377e2d36ce9e0079ec2798
karu1325 <143956677+karu1325@users.noreply.github.com>
2023-10-30 17:08:18 +0000

    [Add]order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,3 +1,19 @@
 class Public::OrdersController < ApplicationController
-  
+
+  def new
+    @order = Order.new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def confirm
+  end
+
+  def create
+  end
+
 end
