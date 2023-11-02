cf8303c645203dec2adc7cab739c0c34a9f5fffb
karu1325 <143956677+karu1325@users.noreply.github.com>
2023-11-02 17:15:38 +0000

    [Add]admin-top,admin-order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,18 @@
 class Admin::OrdersController < ApplicationController
+
+  def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details.all
+  end
+
+  def update
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :payment_method, :billing_amount, :postage,
+                                  :postal_code, :address, :name, :status)
+  end
+
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,11 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
+
+  enum manufacture_status: {
+    impossible: 0,
+    waiting: 1,
+    manufacturing: 2,
+    finish: 3
+  }
 end
