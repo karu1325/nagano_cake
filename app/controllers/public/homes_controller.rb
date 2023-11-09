0ec8fbaae780b36ce8ce6a9c81fa30aeefffeffa
karu1325 <143956677+karu1325@users.noreply.github.com>
2023-11-09 16:23:10 +0000

    [Add]check

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,13 +2,11 @@ class Public::HomesController < ApplicationController
   def top
     @items = Item.all
     @items = Item.order(created_at: :desc).limit(4)
-   
+    @genres = Genre.all
   end
 
   def about
 
   end
-
- 
 
 end
